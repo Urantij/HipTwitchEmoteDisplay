55810d745f6b072bce4d23ee0fe5d022adcaffeb
Urantij <maximumuranti@gmail.com>
2024-11-26 06:10:47 +0700

    Уберёт мусор

--- HipTwitchEmoteDisplay/Program.cs
+++ HipTwitchEmoteDisplay/Program.cs
@@ -42,31 +42,12 @@ builder.Services.AddHostedSingleton<StealthActor>();
 
 WebApplication app = builder.Build();
 
-// var sampleTodos = new Todo[]
-// {
-//     new(1, "Walk the dog"),
-//     new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-//     new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-//     new(4, "Clean the bathroom"),
-//     new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-// };
-
 app.MapHub<LinkHub>("/hub");
 
 app.UseStaticFiles();
 
-// var todosApi = app.MapGroup("/todos");
-// todosApi.MapGet("/", () => sampleTodos);
-// todosApi.MapGet("/{id}", (int id) =>
-//     sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-//         ? Results.Ok(todo)
-//         : Results.NotFound());
-
 app.Run();
 
-// public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-// [JsonSerializable(typeof(Todo[]))]
 [JsonSerializable(typeof(LinkMessage))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
